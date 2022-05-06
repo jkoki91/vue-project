bad61ec0391003c478202e9e8d6b94604b7e0b3e
Jorge Lleonart <jkokeito@gmail.com>
2022-05-06 09:15:19 +0200

    Vue JS

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Este es mi componente</h1>
+    <h2>{{subtitulo}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiComponente", 
+  data() {
+    return {
+      subtitulo: "Soy el subtituto de mi componente",
+    };
+  },
+};
+</script>
